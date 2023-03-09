1c3f2a745513738ab302772404a3fcefbe99dcc6
Ann-Anyango <annmwangombe@gmail.com>
2023-03-09 08:50:22 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,68 @@
+fun main(args: Array<String>) {
+    println(x().min())
+    println(x().max())
+    println(x().average())
+    println(char("Mercy"))
+    println(y().contentToString())
+    addition(34,67)
+    subtraction(45,30)
+    divition(60,5)
+    multiplicatin(5,6)
+    char("love")
+    character("jooy","tom")
+
+
+
+
+}
+fun character(name1:String,name2:String){
+    return
+
+
+}
+fun x ()= arrayOf(1,3,4,6)
+
+
+fun char( char1:String){
+    var char= "love"
+    println(char)
+
+
+}
+fun y()= arrayOf("Stony")
+fun addition(num1:Int,num2:Int){
+    var sum=num1+num2
+    println(sum)
+
+}
+fun subtraction(num3:Int,num4:Int){
+    var sud=num3-num4
+    println(sud)
+}
+fun divition(num5:Int,num6:Int){
+    var div=num5/num6
+    println(div)
+}
+fun multiplicatin(num7:Int,nu8:Int){
+    var mult=num7*nu8
+    println(mult)
+
+}
+
+
+      //  Write and invoke a function that takes in any 2 strings and returns the first
+//character of the longer string or the first character of the first string if they are
+//equal in length.
+//Write and invoke one function that takes in an array of integers and returns
+//these 3 values: smallest, largest and average of all the elements
+//Write and call a function that takes in a string and splits it into all the
+//characters that constitute it. Your function should print out this output.
+//Write and call a function that takes in an array of strings, joins them all into
+//one string and returns it.
+
+//Create a calculator class that is capable of the following functions:
+//(i) Addition of any 2 numbers
+//(ii) Subtraction between any 2 numbers
+//(iii) Accurate division between any 2 numbers
+//(iv) Multiplication of any 2 numbers
+//Instantiate your calculator object and invoke all its functions
